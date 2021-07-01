b2a3c86dd61958b1172374052c39bf887fcb6336
documamushi <socc100917er@icloud.com>
2021-07-02 00:23:12 +0900

    □と○を学習しました

---
+++ 02_レシピ/01_□と○/main.js
@@ -0,0 +1,15 @@
+const figure = document.getElementById("figure")
+// const circleButton = document.getElementById("circle-button")
+// const squareButton = document.getElementById("square-button")
+
+// circleButton.onclick = function() {
+//     figure.classList.add("rounded")
+// }
+
+// squareButton.onckick = function() {
+//     figure.classList.("remove")
+// }
+
+figure.onclick = function() {
+  figure.classList.toggle("rounded")
+}
